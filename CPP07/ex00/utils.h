624059a818b8672d2cda020e9a2e7216cd18afe7
tinitiuset <tinitiuset@gmail.com>
2023-11-15 21:31:42 +0100

    CPP 07 ex00 Done

---
+++ CPP07/ex00/utils.h
@@ -0,0 +1,24 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+const T &min(const T &a, const T &b)
+{
+	return a < b ? a : b;
+}
+
+template <typename T>
+const T &max(const T &a, const T &b)
+{
+	return a > b ? a : b;
+}
+
+#endif

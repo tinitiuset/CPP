416ddbc8170381e4fe2d5b7fd45c4fc071360696
tinitiuset <tinitiuset@gmail.com>
2023-11-24 19:02:20 +0100

    CPP 06 ex02 Revisit

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -24,12 +24,17 @@ Base *generate(void) {
 }
 
 void identify(Base *p) {
-	if (dynamic_cast<A*>(p))
+	if (dynamic_cast<A*>(p) != nullptr) {
 		std::cout << "A" << std::endl;
-	else if (dynamic_cast<B*>(p))
+		return;
+	} else if (dynamic_cast<B*>(p) != nullptr) {
 		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C*>(p))
+		return;
+	} else if (dynamic_cast<C*>(p) != nullptr) {
 		std::cout << "C" << std::endl;
+		return;
+	}
+	std::cout << "Error: Unknown type" << std::endl;
 }
 
 void identify(Base &p) {
